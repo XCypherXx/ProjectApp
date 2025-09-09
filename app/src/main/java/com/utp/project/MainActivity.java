94a495b79dabef9e4a008af65421b288f2feb615
XCypherXx <anthonycondori25@gmail.com>
2025-09-09 00:20:33 -0500

    Splash Screen modificado e icono-name

--- app/src/main/java/com/utp/project/MainActivity.java
+++ app/src/main/java/com/utp/project/MainActivity.java
@@ -1,8 +1,12 @@
 package com.utp.project;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,10 +21,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // logo
 
+        // Referencia a la primera TextView (nameCreater1)
+        TextView nameCreater1 = findViewById(R.id.nameCreater1);
+
+        // Referencia a la segunda TextView (appName)
+        TextView nameCreater2 = findViewById(R.id.nameCreater2);
+
+        // --- Aplica el degradado a la primera TextView (nameCreater1) ---
+        nameCreater1.post(() -> {
+            int width = nameCreater1.getMeasuredWidth();
+            int[] gradientColors = {
+                    Color.parseColor("#FF3632D5"),
+                    Color.parseColor("#FFEFA2FF")
+            };
+            Shader textShader = new LinearGradient(
+                    0, 0,
+                    width, 0,
+                    gradientColors,
+                    null,
+                    Shader.TileMode.CLAMP
+            );
+            nameCreater1.getPaint().setShader(textShader);
+            nameCreater1.invalidate();
+        });
+
+        // --- Aplica el degradado a la segunda TextView (appName) ---
+        nameCreater2.post(() -> {
+            int width = nameCreater2.getMeasuredWidth();
+            int[] gradientColors = {
+                    Color.parseColor("#FF3632D5"),
+                    Color.parseColor("#FFEFA2FF")
+            };
+            Shader textShader = new LinearGradient(
+                    0, 0,
+                    width, 0,
+                    gradientColors,
+                    null,
+                    Shader.TileMode.CLAMP
+            );
+            nameCreater2.getPaint().setShader(textShader);
+            nameCreater2.invalidate();
+        });
+
         new Handler().postDelayed(() -> {
             // Siempre abrir ActivityRegistro después del splash
             startActivity(new Intent(MainActivity.this, ActivityRegistro.class));
             finish(); // cerrar splash
         }, SPLASH_TIME);
+
+
     }
-    }
+
+
+}
